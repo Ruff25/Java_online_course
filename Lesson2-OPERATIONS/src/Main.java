84bc8d037fcd927aba3b4222850b95cff1d6a9c8
Ruff25 <tigerytpubgm@gmail.com>
2025-01-07 18:16:31 +0400

    my first commit

--- Lesson2-OPERATIONS/src/Main.java
+++ Lesson2-OPERATIONS/src/Main.java
@@ -20,6 +20,32 @@ public class Main {
         ++x;
         System.out.println(x);
         System.out.println(x++);
+        System.out.println(x);
+
+
+        x = 4;
+        c = 5;
+        b = x++ - --c; //b = 4 - --c b in veragreluc heto ye x++ anum
+        System.out.println("x = " + x);
+        System.out.println("c = " + c);
+        System.out.println("b = " + b);
+
+        // ++ increment
+        // -- decrement
+
+        boolean bb = x == 5;
+        System.out.println(bb);
+        System.out.println(x != 6);
+        System.out.println(x > 0 && x < 10); // && i depqum ete ka false misht false e tpelu
+
+        boolean vv = false;
+        System.out.println(!vv);
+
+
+        System.out.println(5&6); //hashvel erkuakan@ bazmapatkel heto stacac@ veracel tasakani
+        System.out.println(5|6); //hashvel erkuakan@ gumarel heto stacac@ veracel tasakani
+        System.out.println(5^6); //XOR
+
 
     }
 }
