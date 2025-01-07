4b74920c2dab3f5b7c140bbed6c4b4c959732094
Ruff25 <tigerytpubgm@gmail.com>
2025-01-07 16:42:40 +0400

    first commit

---
+++ Lesson1-TYPES/Lesson1/src/Lesson1.java
@@ -0,0 +1,17 @@
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println();
+
+        byte x = 127; // -128-127
+        short y = 32767;
+        int z = 5656545;
+        long c = 555555555L;
+
+        String firstName = "Hami";
+        String lastName = "Galstyan";
+        System.out.println(firstName + " " + lastName);
+
+        int k = 55;
+        System.out.println("k = " + k);
+    }
+}
