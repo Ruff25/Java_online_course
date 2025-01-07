6a2cf1d26c20c5f31eb3fa9213242b7160413962
Ruff25 <tigerytpubgm@gmail.com>
2025-01-07 17:19:36 +0400

    my first commit

--- Lesson2-OPERATIONS/src/Main.java
+++ Lesson2-OPERATIONS/src/Main.java
@@ -2,5 +2,24 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Wolrd!!");
         System.out.println("Hello Wolrd!!");
+
+        int a = 4, b = 2;
+        int c = a + b;
+        System.out.println("a + b =" + " " + c);
+        c = a - b;
+        System.out.println("a - b =" + " " + c);
+        c = a * b;
+        System.out.println("a * b =" + " " + c);
+        c = a / b;
+        System.out.println("a / b =" + " " + c);
+        System.out.println(a % b); // mnacordner cuyc talis
+
+        int x = 5 + 1;
+        x += 1;
+        x++;
+        ++x;
+        System.out.println(x);
+        System.out.println(x++);
+
     }
 }
